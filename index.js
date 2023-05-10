31ef3d7db4ab834c33ff3b37d3007e969610353c
219WD <macgunmanmc@gmail.com>
2023-05-10 16:13:37 -0300

    Termido el home

---
+++ index.js
@@ -0,0 +1,76 @@
+const toggle = document.querySelector(".toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+toggle.addEventListener("click", () =>{
+    navMenu.classList.toggle("nav-menu_visible");
+})
+
+// SLIDER
+
+var imagenes = [
+    "/assets/1.jpg",
+    "/assets/2.jpg",
+    "/assets/3.jpg",
+];
+document.Imagen.src = imagenes[0];
+
+var sliderDerecha = document.querySelector(".slider-derecha");
+var sliderIzquierda = document.querySelector(".slider-izquierda");
+
+sliderDerecha.addEventListener("click", moverDerecha);
+
+var contador = 0;
+
+function moverDerecha(){
+    contador++;
+    if(contador > imagenes.length - 1)
+    {
+        contador = 0;
+    }
+    document.Imagen.src = imagenes[contador];
+}
+var intervalo = setInterval(moverDerecha, 2000);
+sliderDerecha.addEventListener("click", function(){
+    clearInterval(intervalo);
+    moverDerecha();
+    intervalo = setInterval(moverDerecha, 2000);
+});
+
+function moverIzquierda(){
+    contador--;
+    if(contador < 0){
+        contador = imagenes.length -1;
+    }
+    document.Imagen.src = imagenes[contador];
+}
+sliderIzquierda.addEventListener("click", function(){
+    clearInterval(intervalo);
+    moverIzquierda();
+    intervalo = setInterval(moverIzquierda, 2000);
+
+})
+
+// observador
+
+const imagen1 = document.getElementById('logo');
+const imagen2 = document.getElementById('img-mc');
+
+const cargarImagen = (entradas, observador) => {
+    // console.log(entradas)
+    // console.log(observador)
+
+    entradas.forEach((entrada) => {
+        if(entrada.isIntersecting){
+            entrada.target.classList.add('visible');
+        }
+    })
+}
+
+const observador = new IntersectionObserver(cargarImagen, {
+    root: null,
+    rootMargin: '500px 0px 0px 0px',
+    threshold: 0.5
+});
+
+observador.observe(imagen1);
+observador.observe(imagen2);
